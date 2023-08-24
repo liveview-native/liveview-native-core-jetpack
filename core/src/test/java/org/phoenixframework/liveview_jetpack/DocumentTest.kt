5eed221e4eb9819391df648eab1925a07a417060
Bajix <Thomas@bajix.com>
2023-08-24 07:06:23 -0700

    Add morphing test

--- core/src/test/java/org/phoenixframework/liveview_jetpack/DocumentTest.kt
+++ core/src/test/java/org/phoenixframework/liveview_jetpack/DocumentTest.kt
@@ -14,4 +14,32 @@ class DocumentTest {
     fun it_constructs_empty_native_doc() {
         Document()
     }
+
+    @Test
+    fun it_morphs_live_form() {
+        var doc = Document.parse("""
+        <VStack modifiers="">
+            <VStack>
+                <LiveForm id="login" phx-submit="login">
+                    <TextField name="email" modifiers="">
+                        Email
+                    </TextField>
+                    <LiveSubmitButton modifiers="">
+                        <Text>Enter</Text>
+                    </LiveSubmitButton>
+                </LiveForm>
+            </VStack>
+        </VStack>
+        """);
+
+        var to = Document.parse("""
+        <VStack modifiers="">
+            <VStack>
+                <Text>Success! Check your email for magic link</Text>
+            </VStack>
+        </VStack>
+        """);
+
+        doc.merge(to,  Document.Companion.Handler());
+    }
 }
