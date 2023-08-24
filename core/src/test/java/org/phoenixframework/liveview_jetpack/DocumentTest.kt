73571af858182c27504b9a38007a8658a33a2938
Bajix <Thomas@bajix.com>
2023-08-24 05:22:12 -0700

    Add test for constructing native Document

--- core/src/test/java/org/phoenixframework/liveview_jetpack/DocumentTest.kt
+++ core/src/test/java/org/phoenixframework/liveview_jetpack/DocumentTest.kt
@@ -1,17 +1,17 @@
-package org.phoenixframework.liveview_jetpack
+
 
 import org.junit.Test
-
 import org.junit.Assert.*
+import org.phoenixframework.liveview.lib.Document
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class DocumentTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun it_constructs_empty_native_doc() {
+        Document()
     }
 }
