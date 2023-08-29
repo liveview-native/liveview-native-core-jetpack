2bbb6a85d23a25f87717daeef3b7242164db2689
Thomas Sieverding <Thomas@bajix.com>
2023-08-29 13:52:17 -0700

    Clean up docs
    Co-authored-by: Elle Imhoff <Kronic.Deth@gmail.com>

--- core/src/test/java/org/phoenixframework/liveview_jetpack/DocumentTest.kt
+++ core/src/test/java/org/phoenixframework/liveview_jetpack/DocumentTest.kt
@@ -4,11 +4,6 @@ import org.junit.Test
 import org.junit.Assert.*
 import org.phoenixframework.liveview.lib.Document
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class DocumentTest {
     @Test
     fun it_constructs_empty_native_doc() {
