68d947e64de2f925b8f9ced149d912bc64043be4
Nelson Glauber <nelson.glauber@dockyard.com>
2023-09-14 20:56:42 -0300

    Fix Jitpack

---
+++ scripts/prepareJitpackEnvironment.sh
@@ -0,0 +1,5 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+rustup target add armv7-linux-androideabi
+rustup target add i686-linux-android
+rustup target add aarch64-linux-android
+rustup target add x86_64-linux-android
