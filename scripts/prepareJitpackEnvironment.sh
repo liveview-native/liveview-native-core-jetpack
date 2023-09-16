de8ff89879e0823b794bbee4ca41cf741685c24d
Nelson Glauber <nelson.glauber@dockyard.com>
2023-09-16 20:45:13 -0300

    Trying to install RUST

--- scripts/prepareJitpackEnvironment.sh
+++ scripts/prepareJitpackEnvironment.sh
@@ -1,4 +1,4 @@
-sudo apt install rustc
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup target add armv7-linux-androideabi
 rustup target add i686-linux-android
 rustup target add aarch64-linux-android
