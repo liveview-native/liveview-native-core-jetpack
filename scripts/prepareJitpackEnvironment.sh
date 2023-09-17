fd03fdddfe599846126dcb70f1d5e7272a4cf79d
Nelson Glauber <nelson.glauber@dockyard.com>
2023-09-16 21:21:10 -0300

    Trying to install Rust 3

--- scripts/prepareJitpackEnvironment.sh
+++ scripts/prepareJitpackEnvironment.sh
@@ -1,4 +1,5 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
 rustup target add armv7-linux-androideabi
 rustup target add i686-linux-android
 rustup target add aarch64-linux-android
