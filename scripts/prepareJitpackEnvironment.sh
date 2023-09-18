45aeb3d62c3292ca2daa9a4cdea7e67f372cfea8
Nelson Glauber <nelson.glauber@dockyard.com>
2023-09-18 10:16:20 -0300

    Fix linker error 2

--- scripts/prepareJitpackEnvironment.sh
+++ scripts/prepareJitpackEnvironment.sh
@@ -11,4 +11,4 @@ rustup target add i686-linux-android
 rustup target add aarch64-linux-android
 rustup target add x86_64-linux-android
 # Fix error: linker `cc` not found
-apt install build-essential
+sudo apt install build-essential
