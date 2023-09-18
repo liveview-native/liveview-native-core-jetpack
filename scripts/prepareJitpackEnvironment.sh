e3b1011d7bf55569b49538e55a7205f6afefacdb
Nelson Glauber <nelson.glauber@dockyard.com>
2023-09-17 22:25:40 -0300

    Compiling JNI libs using cargo 16

--- scripts/prepareJitpackEnvironment.sh
+++ scripts/prepareJitpackEnvironment.sh
@@ -6,3 +6,6 @@ rustup target add armv7-linux-androideabi
 rustup target add i686-linux-android
 rustup target add aarch64-linux-android
 rustup target add x86_64-linux-android
+export RUST_ANDROID_GRADLE_RUSTC_COMMAND="$HOME/.cargo/bin/rustc"
+export RUST_ANDROID_GRADLE_CARGO_COMMAND="$HOME/.cargo/bin/cargo"
+export RUST_ANDROID_GRADLE_RUSTUP_CHANNEL="nightly"
